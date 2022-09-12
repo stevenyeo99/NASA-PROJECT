fe440a966217c71ea5bafed7b21192f87508f80a
steven.yeo <steven.y@dynrtech.com>
2022-09-12 21:52:25 +0700

    Add Load Planet data

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,10 +2,12 @@ const request = require('supertest');
 
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Test Launches API', () => {
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
